06b31565df45cae8d78cbab55932ec23722a71b7
Le5ya <liliatolstyh@yandex.ru>
2023-12-18 11:43:45 +0300

    adaptive

--- js/script.js
+++ js/script.js
@@ -2,8 +2,17 @@ const swiperThumb = new Swiper('.gift__swiper_thumb', {
 	spaceBetween: 16,
 	slidesPerView: 6,
 	freeMode: true,
+	breakpoints: {
+		320: {
+			slidesPerView: 5,
+			spaceBetween: 12,
+		},
+		1141: {
+			spaceBetween: 16,
+			slidesPerView: 6,
+		}
+	},
 	watchSlidesProgress: true,
-  
 });
 const swiperMain = new Swiper('.gift__swiper_card', {
 	spaceBetween: 16,
